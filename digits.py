f7e1039f250eb39bd3cd380c299f62184e48e5aa
Hampus <hampus.p.larsson@gmail.com>
2019-10-08 09:13:56 +0200

    Added main file

---
+++ digits.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+if __name__ == '__main__' :
+
+    print('Neural Network')
+
+
+    # train a network of perceptrons (no hidden
+    # layers) with the help of patterns (images) and
+    # answers (labels)
+    # – classify a test set of patterns and return the
+    # classifications
+    # – The input patterns are stored in a training
+    # image file
+    # – The correct classifications for these are stored
+    # in a training label file
+    # – The patterns you should classify are stored in
+    # a validation image file – consists of new
+    # images
+    # – The answers we use to check your result is
+    # called the validation label file
