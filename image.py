8b9e074081e4dfc156da0e60f2f9c23d2059c305
Hampus <hampus.p.larsson@gmail.com>
2019-10-10 12:19:48 +0200

    Fixed image init

--- image.py
+++ image.py
@@ -5,6 +5,7 @@ class Image:
 
     def __init__(self, image):
         self.array = np.array(map(0.001.__mul__, map(int, image.split(' '))))
+        self.label = None
 
     def add_label(self, label):
         self.label = label
